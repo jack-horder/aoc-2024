008f867116548ec8f86c9c94debc6ecbcc99b869
Jack Horder <jackwjhorder@gmail.com>
2024-12-31 03:37:16 +0200

    cleaned up day 2

--- day_2/day2.py
+++ day_2/day2.py
@@ -1,14 +1,12 @@
-from ast import Tuple
 import copy
-from typing import Tuple
 
 
 def parse_report(file_name:str) -> list[list[int]]:
     reports:list[list[int]] = []
     with open(file=file_name, mode="r") as f:
         lines = f.readlines()
-        for l in lines:
-            reports.append(list(map(lambda x: int(x), l.split(sep=" "))))
+        for line in lines:
+            reports.append(list(map(lambda x: int(x), line.split(sep=" "))))
     return reports
 
 def report_check(report:list[int]) -> bool:
@@ -24,27 +22,11 @@ def report_check(report:list[int]) -> bool:
             return False
     return True
 
-def report_check(report:list[int]) -> Tuple[bool, int]:
-    ascending = False
-    for i in range(len(report)):
-        if i == 0:
-            continue
-        diff = report[i] - report[i-1]
-        if abs(diff) > 3 or diff == 0:
-            return False, i
-        if i == 1:
-            ascending = (diff < 0)
-            continue
-        if ascending != (diff < 0):
-            return False, i
-    return True, i
-
 def day_2_part_1():
     reports = parse_report("day_2/puzzle_input.txt")
     safe_reports_count = 0
     for r in reports:
-        check, _ = report_check(r)
-        if check:
+        if report_check(r):
             print(r)
             safe_reports_count +=1
     print("Safe Reports Count:")
@@ -54,16 +36,14 @@ def day_2_part_2():
     reports = parse_report("day_2/puzzle_input.txt")
     safe_reports_count = 0
     for r in reports:
-        check, _ = report_check(r)
-        if check:
+        if report_check(r):
             print(r)
             safe_reports_count +=1
             continue
         for c in range(len(r)):
             r_copy = copy.copy(r)
             del r_copy[c]
-            check, _ = report_check(r_copy)
-            if check:
+            if report_check(r_copy):
                 print(r_copy)
                 safe_reports_count +=1
                 break
@@ -77,5 +57,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
